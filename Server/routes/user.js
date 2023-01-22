40746ebb36441b4d3f457a459968095a2b83568f
Andrew <44222520+andrewjthomsen@users.noreply.github.com>
2023-01-21 17:01:21 -0800

    Began adding user login schema

--- Server/routes/user.js
+++ Server/routes/user.js
@@ -1,9 +1,25 @@
 const express = require('express');
 const router = express.Router();
 
-router.get("/signup", (req, res) => {
-   res.send("Hello from Node");
-});
+// const { requireSignin, isAuth, isAdmin } = require('../controllers/auth');
+//
+// const { userById, read, update, purchaseHistory } = require('../controllers/user');
 
+// router.get('/secret', requireSignin, (req, res) => {
+// router.get((req, res) => {
+//     res.json({
+//         user: 'got here yay'
+//     });
+// });
+const {sayHi} = require("../controllers/user");
+router.get("/", sayHi);
+
+
+
+// router.get('/user/:userId', requireSignin, isAuth, read);
+// router.put('/user/:userId', requireSignin, isAuth, update);
+// router.get('/orders/by/user/:userId', requireSignin, isAuth, purchaseHistory);
+//
+// router.param('userId', userById);
 
 module.exports = router;
--- Server/server.js
+++ Server/server.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const mongoose = require('mongoose');
-// const morgan = require('morgan');
-// const bodyParser = require('body-parser');
-// const cookieParser = require('cookie-parser');
-// const cors = require('cors');
-// const expressValidator = require('express-validator');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+const expressValidator = require('express-validator');
 require('dotenv').config();
 // import routes
 
@@ -23,11 +23,11 @@ mongoose
     .then(() => console.log('DB Connected'));
 
 // middlewares
-// app.use(morgan('dev'));
-// app.use(bodyParser.json());
-// app.use(cookieParser());
-// // app.use(expressValidator());
-// app.use(cors());
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(cookieParser());
+// app.use(expressValidator());
+app.use(cors());
 
 // routes middleware
 app.use(userRoutes);
