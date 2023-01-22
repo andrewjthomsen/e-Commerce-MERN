ca6d05ef9c51d1a7633ed6822f5df23e974c1149
Andrew <44222520+andrewjthomsen@users.noreply.github.com>
2023-01-21 17:26:29 -0800

    further built out user schema

--- Server/models/user.js
+++ Server/models/user.js
@@ -13,7 +13,8 @@ const userSchema = new mongoose.Schema({
             type: String,
             trim: true,
             required: true,
-            unique: 32
+            unique: true,
+            lowercase: true
         },
         hashed_password: {
             type: String,
@@ -34,3 +35,34 @@ const userSchema = new mongoose.Schema({
         }
     }, {timestamps: true}
 );
+
+// virtual field
+userSchema.virtual('password')
+    .set(function(password) {
+        // create a temporarity variable called _password
+        this._password = password
+        // generate salt
+        this.salt = uuidv1()
+        // encryptPassword
+        this.hashed_password = this.encryptPassword(password);
+    })
+    .get(function() {
+        return this._password;
+    })
+
+userSchema.methods = {
+    encryptPassword: function(password) {
+        if (!password) return '';
+        try {
+            return crypto
+                .createHmac('sha1', this.salt)
+                .update(password)
+                .digest('hex');
+        } catch (err) {
+            return '';
+        }
+    },
+}
+
+
+module.exports = mongoose.model('User', userSchema);
