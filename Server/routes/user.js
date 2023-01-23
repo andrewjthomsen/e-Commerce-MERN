7ccab0152b8e12134657414f4f540583a0eb0c34
Andrew <44222520+andrewjthomsen@users.noreply.github.com>
2023-01-22 23:58:29 -0800

    Modified files to get signin working

--- Server/routes/user.js
+++ Server/routes/user.js
@@ -1,28 +1,13 @@
 const express = require('express');
 const router = express.Router();
 
-// const { requireSignin, isAuth, isAdmin } = require('../controllers/auth');
+
 const {signup, signin, signout} = require("../controllers/user");
 const {userSignupValidator} = require('../validator/index');
-// const { userById, read, update, purchaseHistory } = require('../controllers/user');
-
-// router.get('/secret', requireSignin, (req, res) => {
-// router.get((req, res) => {
-//     res.json({
-//         user: 'got here yay'
-//     });
-// });
 
 router.post("/signup", userSignupValidator, signup);
 router.post("/signin", signin);
 router.get("/signout", signout);
 
-
-
-// router.get('/user/:userId', requireSignin, isAuth, read);
-// router.put('/user/:userId', requireSignin, isAuth, update);
-// router.get('/orders/by/user/:userId', requireSignin, isAuth, purchaseHistory);
-//
-// router.param('userId', userById);
 
 module.exports = router;
